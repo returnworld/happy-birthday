1a9d1173cb242dfbd5ebbe2ac900113997a2dc02
returnworld <dummok@yandex.ru>
2024-10-13 20:29:13 +0500

    fix path

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import image from "@rollup/plugin-image"
+import { defineConfig } from 'vite'
 
 export default {
+  base: './',
+  build: {
+    outDir: './docs',
+    emptyOutDir: true, // also necessary
+  },
   plugins: [
     {
-      //...image(),
-      //enforce: 'pre',
+      ...image(),
+      enforce: 'pre',
     },
   ]
 }
